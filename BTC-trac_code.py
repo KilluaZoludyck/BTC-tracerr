c217c6d72178fb1527dcba68e20e8db29eda4c40
KilluaZoludyck <144722702+KilluaZoludyck@users.noreply.github.com>
2025-02-09 13:26:53 +0300

    BTC-tracer

--- BTC-trac_code.py
+++ BTC-trac_code.py
@@ -5,23 +5,27 @@ sleep = 3
 
 
 def update_ticker():
-    BTC = "https://www.rbc.ru/crypto/currency/btcusd"
-    headers = {
-            'user agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                          "AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/131.0.0.0 Safari/537.36"}
+    try:
+        BTC = "https://www.rbc.ru/crypto/currency/btcusd"
+        headers = {
+                'user agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                              "AppleWebKit/537.36 (KHTML, like Gecko) "
+                              "Chrome/131.0.0.0 Safari/537.36"}
 
 
-    html = requests.get(BTC, headers)
-    soap = BeautifulSoup(html.content, "html.parser")
-    convert = soap.findAll("div", {"class": "chart__subtitle js-chart-value"})
-    convert_list = list(convert[0])
+        html = requests.get(BTC, headers)
+        soap = BeautifulSoup(html.content, "html.parser")
+        convert = soap.findAll("div", {"class": "chart__subtitle js-chart-value"})
+        convert_list = list(convert[0])
 
-    price = str(convert_list[0]).strip()
-    print(f'BitCoin now: {price}$\n')
+        price = str(convert_list[0]).strip()
+        print(f'BitCoin now: {price}$\n')
 
-    time.sleep(sleep)
-    update_ticker()
+        time.sleep(sleep)
+        update_ticker()
+
+    except BaseException("Something went wrong."):
+        print("Что-то пошло не так")
 
 
 update_ticker()
